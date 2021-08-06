00258c15d84af54d0b6b019ef64c32930003ca63
tabrasel <tatebrasel@gmail.com>
2021-08-05 17:02:33 -0700

    feat: show participant icons in round-info component

--- src/app/round-info/round-info.component.ts
+++ src/app/round-info/round-info.component.ts
@@ -1,7 +1,11 @@
 import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 import { IAlbum } from '../interfaces/IAlbum';
+import { IMember } from '../interfaces/IMember';
 import { IRound } from '../interfaces/IRound';
+
+import { AlbumService } from '../album.service';
+import { MemberService } from '../member.service';
 
 @Component({
   selector: 'app-round-info',
@@ -11,10 +15,14 @@ import { IRound } from '../interfaces/IRound';
 export class RoundInfoComponent implements OnInit {
 
   selectedAlbum: IAlbum;
+  participants: IMember[];
 
   @Input() round: IRound;
 
-  constructor() { }
+  constructor(
+    private albumService: AlbumService,
+    private memberService: MemberService
+  ) { }
 
   ngOnInit(): void {
     this.selectedAlbum = null;
@@ -23,11 +31,30 @@ export class RoundInfoComponent implements OnInit {
   ngOnChanges(changes: SimpleChanges): void {
     if ('round' in changes) {
       this.selectedAlbum = null;
+      this.loadParticipants();
     }
   }
 
   selectAlbum(newSelectedAlbum: any): void {
     this.selectedAlbum = newSelectedAlbum;
+  }
+
+  async loadParticipants(): Promise<void> {
+    this.participants = [];
+    for (let albumId of this.round.albumIds) {
+      const album: IAlbum = await this.albumService.getAlbumById(albumId).toPromise();
+      const poster: IMember = await this.memberService.getMemberById(album.posterId).toPromise();
+      this.participants.push(poster);
+    }
+
+    // Sort participants by last name, first name
+    this.participants.sort((a, b) => {
+      if (a.lastName < b.lastName)
+        return -1;
+      else if (a.lastName > b.lastName)
+        return 1;
+      return a.firstName < b.firstName ? -1 : 1;
+    });
   }
 
 }
